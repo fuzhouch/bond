2e1b970d440e0674a5d58f52927ea6f8fe262968
Christopher Warrington <chwarr@microsoft.com>
2017-03-29 18:04:41 -0700

    [c#] Add Bond-over-gRPC
    * gbc can now generate proxies and service stubs for gRPC
    * The assembly Bond.Grpc.dll contains glue code
    * Documentation updated
        * Bond Comm deprecation notice added
    
    Closes https://github.com/Microsoft/bond/pull/386

--- compiler/src/Language/Bond/Codegen/Templates.hs
+++ compiler/src/Language/Bond/Codegen/Templates.hs
@@ -55,7 +55,7 @@ module Language.Bond.Codegen.Templates
     , KtFieldMapping(..)
     , KtStructMapping(..)
     , types_kt
-
+    , grpc_cs
     )
     where
 
@@ -71,6 +71,7 @@ import Language.Bond.Codegen.Cpp.Comm_h
 import Language.Bond.Codegen.Cs.Types_cs
 import Language.Bond.Codegen.Cs.Comm_cs
 import Language.Bond.Codegen.Kt.Types_kt
+import Language.Bond.Codegen.Cs.Grpc_cs
 -- redundant imports for haddock
 import Language.Bond.Codegen.TypeMapping
 import Language.Bond.Syntax.Types
