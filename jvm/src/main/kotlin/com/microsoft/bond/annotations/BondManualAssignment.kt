54c25fd995e6c5701527cc08c58ab618ac46334e
Fuzhou Chen <fuzhouch@microsoft.com>
2017-04-29 21:51:09 +0800

    [Fix] Adjust documentation of BondManualAssignment.

--- jvm/src/main/kotlin/com/microsoft/bond/annotations/BondManualAssignment.kt
+++ jvm/src/main/kotlin/com/microsoft/bond/annotations/BondManualAssignment.kt
@@ -4,21 +4,19 @@
 
 package com.microsoft.bond.annotations
 
-import kotlin.reflect.KClass
-
 /**
  * Mark a field that must be assigned manually.
  *
- * This is a field to be used in a Bond struct with generic type like
- * below. Bond semantic requires a default value of T object created
+ * This is a tag attribute on a field with generic type. Bond's
+ * semantic requires a default value of T object initialized
  * when creating Record. Both C++ and C# can do this, but Java/Kotlin
  * does not, due to the limitation of Java/Kotlin generic.
  *
  * There's no way right now to support "new T()" syntax. So we have to
- * apply a less guarantee: we assign this field to null by default, and
+ * apply a weak guarantee: we assign this field to null by default, and
  * inform serializer to throw runtime exception, if the field is not
- * assigned a valid object. This is weaker than C++/C# approach, but
- * guarantees validity of sent buffer.
+ * assigned a valid object. This is less powerful than C++/C#
+ * approach, but it guarantees correctness of sent buffer.
  *
  * struct Record<T>
  * {
