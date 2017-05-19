a4c74ab8cfeaebe3b335fbc06dd97a13d5278da8
Fuzhou Chen <fuzhouch@microsoft.com>
2017-05-19 16:48:14 +0800

    [New] Add skeleton code for decoding part.

---
+++ jvm/src/main/kotlin/com/microsoft/bond/io/InputBuffer.kt
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root
+// for full license information.
+
+package com.microsoft.bond.io
+
+/**
+ * Created by fuzhouch on 5/19/2017.
+ */
