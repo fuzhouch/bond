5ce1ae02b7ed051d98c8241a51c1e3a3f31aabd1
Fuzhou Chen <fuzhouch@microsoft.com>
2017-05-25 18:35:00 +0800

    [Fix] Refactor code for readability.
    Use lambdas with shorter parameters.

--- jvm/src/main/kotlin/com/microsoft/jbond/Deserializer.kt
+++ jvm/src/main/kotlin/com/microsoft/jbond/Deserializer.kt
@@ -11,12 +11,14 @@ import com.microsoft.jbond.types.*
 import java.lang.reflect.Field
 import java.util.TreeMap
 
-internal data class FieldSetterInfo<T>(val field: Field, val setter: (T, Field, TaggedProtocolReader) -> Unit)
+internal data class FieldSetterInfo<T>(val field: Field, val readValueOp: (TaggedProtocolReader) -> Any) {
+    fun setFieldValue(obj: T, reader: TaggedProtocolReader) = field.set(obj, readValueOp(reader))
+}
 
 internal class TaggedClassDeserializer<T>(klass: Class<T>) {
     // TODO: Haven't supported inheritance yet.
     val cls = klass
-    val fieldsMap = TreeMap<Int, FieldSetterInfo<T>>()
+    private val fieldsMap = TreeMap<Int, FieldSetterInfo<T>>()
 
     init {
         if (!isBondGenerated(cls)) {
@@ -27,44 +29,41 @@ internal class TaggedClassDeserializer<T>(klass: Class<T>) {
             val fieldId = it.getDeclaredAnnotation(BondFieldId::class.java).id
             val fieldType = it.type
             it.isAccessible = true
-            fieldsMap.put(fieldId, FieldSetterInfo(it, createFieldSetter(fieldType)))
+            fieldsMap.put(fieldId, FieldSetterInfo(it, createFieldGetter(fieldType)))
         }
         // TODO: Add support for cls.superclass.declaredFields later.
     }
 
-    private fun createFieldSetter(fieldType: Class<*>): (T, Field, TaggedProtocolReader) -> Unit {
-        return when (fieldType) {
-            Boolean::class.java -> { obj, field, reader -> field.set(obj, reader.readBool()) }
-            Byte::class.java -> { obj, field, reader -> field.set(obj, reader.readInt8()) }
-            Short::class.java -> { obj, field, reader -> field.set(obj, reader.readInt16()) }
-            Int::class.java -> { obj, field, reader -> field.set(obj, reader.readInt32()) }
-            Long::class.java -> { obj, field, reader -> field.set(obj, reader.readInt64()) }
-            UnsignedByte::class.java -> { obj, field, reader -> field.set(obj, reader.readUInt8()) }
-            UnsignedShort::class.java -> { obj, field, reader -> field.set(obj, reader.readUInt16()) }
-            UnsignedInt::class.java -> { obj, field, reader -> field.set(obj, reader.readUInt32()) }
-            UnsignedLong::class.java -> { obj, field, reader -> field.set(obj, reader.readUInt64()) }
-            ByteString::class.java -> { obj, field, reader -> field.set(obj, reader.readByteString()) }
-            String::class.java -> { obj, field, reader -> field.set(obj, reader.readUnicodeString()) }
-            else -> throw UnsupportedBondTypeException(fieldType.toString())
-        }
+    fun deserialize(obj: T, reader: TaggedProtocolReader): T {
+        fieldsMap.forEach { it.value.setFieldValue(obj, reader) }
+        return obj
     }
 
-    fun deserialize(obj: T, reader: TaggedProtocolReader): T {
-        fieldsMap.forEach { it.value.setter(obj, it.value.field, reader) }
-        return obj
+    private fun createFieldGetter(fieldType: Class<*>): (TaggedProtocolReader) -> Any {
+        return when (fieldType) {
+            Boolean::class.java -> { reader -> reader.readBool() }
+            Byte::class.java -> { reader -> reader.readInt8() }
+            Short::class.java -> { reader -> reader.readInt16() }
+            Int::class.java -> { reader -> reader.readInt32() }
+            Long::class.java -> { reader -> reader.readInt64() }
+            UnsignedByte::class.java -> { reader -> reader.readUInt8() }
+            UnsignedShort::class.java -> { reader -> reader.readUInt16() }
+            UnsignedInt::class.java -> { reader -> reader.readUInt32() }
+            UnsignedLong::class.java -> { reader -> reader.readUInt64() }
+            ByteString::class.java -> { reader -> reader.readByteString() }
+            String::class.java -> { reader -> reader.readUnicodeString() }
+            else -> throw UnsupportedBondTypeException(fieldType.toString())
+        }
     }
 }
 
 /**
- * Deserialize objects of given type
+ * Deserialize objects of given type.
  */
 class Deserializer<T>(klass: Class<T>) {
-    // TODO: Haven't supported inheritance yet.
     val cls = klass
     private val tagged = TaggedClassDeserializer<T>(cls)
     // TODO Support untagged deserizlizer later.
 
-    fun deserialize(taggedReader: TaggedProtocolReader): T {
-        return tagged.deserialize(cls.newInstance(), taggedReader)
-    }
+    fun deserialize(reader: TaggedProtocolReader): T = tagged.deserialize(cls.newInstance(), reader)
 }
