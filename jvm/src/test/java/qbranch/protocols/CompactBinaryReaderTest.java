f35d3de2a0efa1c97b29a5f0d2dbced8e49527ad
Fuzhou Chen <fuzhouch@microsoft.com>
2017-06-05 14:19:30 +0800

    [Fix] Decoding integers really works now.
    It's not a problem of my code, but test data. The encoded string
    in testidl_*.txt was not correct. Now it's fixed and everything works.

--- jvm/src/test/java/qbranch/protocols/CompactBinaryReaderTest.java
+++ jvm/src/test/java/qbranch/protocols/CompactBinaryReaderTest.java
@@ -12,6 +12,7 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.lang.instrument.Instrumentation;
+import java.math.BigInteger;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -45,7 +46,12 @@ public class CompactBinaryReaderTest {
         Assert.assertEquals(intStruct.getInt8value(), 0x01);
         Assert.assertEquals(intStruct.getInt16value(), 0x0101);
         Assert.assertEquals(intStruct.getInt32value(), 0x01010101);
-        // Assert.assertEquals(intStruct.getInt64value(), 0x0101010101010101L);
+        Assert.assertEquals(intStruct.getInt64value(), 0x0101010101010101L);
+
+        Assert.assertEquals(intStruct.getUint8value().getValue(), (short)0x01);
+        Assert.assertEquals(intStruct.getUint16value().getValue(), 0x0101);
+        Assert.assertEquals(intStruct.getUint32value().getValue(), (long)0x01010101);
+        Assert.assertEquals(intStruct.getUint64value().getValue(), new BigInteger("0101010101010101", 16));
     }
 
     @Test(expected=UnsupportedBondTypeException.class)
